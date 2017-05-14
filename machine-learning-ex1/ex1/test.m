27f7dfafa53250b319ed7870be491465e81769cc
Devesh Arora <devesharora@Deveshs-MacBook-Pro.local>
2017-05-13 21:24:38 -0400

    assignment

---
+++ machine-learning-ex1/ex1/test.m
@@ -0,0 +1,28 @@
+fprintf('Plotting Data ...\n')
+data = load('ex1data1.txt');
+X = data(:, 1); y = data(:, 2);
+m = length(y); % number of training examples
+
+X;
+y;
+m;
+
+
+% Plot Data
+% Note: You have to complete the code in plotData.m
+%plotData(X, y);
+
+
+
+X = [ones(m, 1), data(:,1)]; % Add a column of ones to x
+theta = zeros(2, 1); % initialize fitting parameters
+
+% Some gradient descent settings
+iterations = 1500;
+alpha = 0.01;
+
+fprintf('\nTesting the cost function ...\n')
+% compute and display initial cost
+J = computeCost(X, y, theta);
+fprintf('With theta = [0 ; 0]\nCost computed = %f\n', J);
+fprintf('Expected cost value (approx) 32.07\n');
