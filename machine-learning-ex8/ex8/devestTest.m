aa44e99075d0602848d3c4ef25ca998e474e9b09
Devesh Arora <devesharora@Deveshs-MacBook-Pro.local>
2017-05-21 21:09:33 -0400

    Week 9 Assignment

---
+++ machine-learning-ex8/ex8/devestTest.m
@@ -0,0 +1,27 @@
+
+%The following command loads the dataset. You should now have the
+%variables X, Xval, yval in your environment
+load('ex8data1.mat');
+
+%  Visualize the example dataset
+plot(X(:, 1), X(:, 2), 'bx');
+axis([0 30 0 30]);
+xlabel('Latency (ms)');
+ylabel('Throughput (mb/s)');
+
+fprintf('Program paused. Press enter to continue.\n');
+pause
+
+
+%% ================== Part 2: Estimate the dataset statistics ===================
+%  For this exercise, we assume a Gaussian distribution for the dataset.
+%
+%  We first estimate the parameters of our assumed Gaussian distribution, 
+%  then compute the probabilities for each of the points and then visualize 
+%  both the overall distribution and where each of the points falls in 
+%  terms of that distribution.
+%
+fprintf('Visualizing Gaussian fit.\n\n');
+
+%  Estimate my and sigma2
+[mu sigma2] = estimateGaussian(X);
